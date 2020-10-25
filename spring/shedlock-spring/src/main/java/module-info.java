f7d0894c946673160ac0c850c9d4139c25c94731
Lukas Krecan <lukas@krecan.net>
2020-10-25 10:25:04 +0100

    Introduce Java 9 modules

---
+++ spring/shedlock-spring/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module net.javacrumbs.shedlock.spring {
+    requires org.slf4j;
+    requires net.javacrumbs.shedlock.core;
+    requires spring.beans;
+    requires spring.context;
+    requires spring.core;
+    requires spring.aop;
+    opens net.javacrumbs.shedlock.spring.aop to spring.beans;
+}
